205b52a02cb35adb293b118bf9643107aaad9dc3
Abhirup Ghosh <47599858+abhirup-ghosh@users.noreply.github.com>
2024-06-17 16:27:00 +0000

    updated hw4

---
+++ cohorts/2024/04-deployment/homework/starter_ag.py
@@ -0,0 +1,61 @@
+#!/usr/bin/env python
+# coding: utf-8
+
+
+import pickle
+import pandas as pd
+import numpy as np
+import sys
+
+with open('model.bin', 'rb') as f_in:
+    dv, model = pickle.load(f_in)
+
+categorical = ['PULocationID', 'DOLocationID']
+
+def read_data(filename):
+    df = pd.read_parquet(filename)
+    
+    df['duration'] = df.tpep_dropoff_datetime - df.tpep_pickup_datetime
+    df['duration'] = df.duration.dt.total_seconds() / 60
+
+    df = df[(df.duration >= 1) & (df.duration <= 60)].copy()
+
+    df[categorical] = df[categorical].fillna(-1).astype('int').astype('str')
+    
+    return df
+
+
+year = int(sys.argv[1]) # 2023
+month = int(sys.argv[2]) # 3
+taxi_type = 'yellow'
+
+df = read_data(f'https://d37ci6vzurychx.cloudfront.net/trip-data/{taxi_type}_tripdata_{year:04d}-{month:02d}.parquet')
+
+
+dicts = df[categorical].to_dict(orient='records')
+X_val = dv.transform(dicts)
+y_pred = model.predict(X_val)
+
+df['ride_id'] = f'{year:04d}/{month:02d}_' + df.index.astype('str')
+
+
+df_result = pd.DataFrame()
+df_result['ride_id'] = df['ride_id']
+# df_result['tpep_pickup_datetime'] = df['tpep_pickup_datetime']
+# df_result['PULocationID'] = df['PULocationID']
+# df_result['DOLocationID'] = df['DOLocationID']
+# df_result['actual_duration'] = df['duration']
+df_result['predicted_duration'] = y_pred
+# df_result['diff'] = df_result['actual_duration'] - df_result['predicted_duration']
+# df_result['model_version'] = df['ride_id']
+
+print('standard deviation of the predicted duration for this dataset:', np.std(df_result['predicted_duration']))
+print('mean of the predicted duration for this dataset:', np.mean(df_result['predicted_duration']))
+
+output_file = f'output_{taxi_type}_{year:04d}-{month:02d}.parquet'
+df_result.to_parquet(
+    output_file,
+    engine='pyarrow',
+    compression=None,
+    index=False
+)
